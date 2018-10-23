e62a1a2bd5630416a39dbb101b2c1d954f5974dd
rossumg <rossumg@uw.edu>
2018-10-23 15:50:02 -0700

    db.create_all()

--- webapp/tbapi/tbapi_app.py
+++ webapp/tbapi/tbapi_app.py
@@ -4,23 +4,24 @@ from flask_sqlalchemy import SQLAlchemy
 #from tbapi.models.oauth2 import OAuth2Client
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://tbapi:tbapi@tbapp-db:3306'
-#db = SQLAlchemy(app)
-
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://tbapi:tbapi@tbapp-db:3306'
+db = SQLAlchemy(app)
 
 @app.shell_context_processor
 def make_shell_context():
     return dict(app=app, db=db)
 
 def init_db():
-
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://tbapi:tbapi@tbapp-db:3306/tbapi'
+    db = SQLAlchemy(app)
+   
     class AlembicVersion(db.Model):
         __tableName__ = 'alembic_version'
         version_num = db.Column(db.String(32), primary_key=True, nullable=False)
 
     class OAuth2Client(db.Model):
         __tablename__ = 'oauth2_client'
-
+        
         client_id = db.Column(db.String(48), nullable=True)
         client_secret = db.Column(db.String(120), nullable=False)
         is_confidential = db.Column(db.Integer, nullable=False)
@@ -31,7 +32,7 @@ def init_db():
         user_id = db.Column(db.Integer, nullable=False)
         name = db.Column(db.String(48), nullable=False)
         website = db.Column(db.Text)
-        allowed_grants = db.Column(db.Text)
+        allowed_grants = db.Column(db.Text)    
 
     class OAuth2AuthorizationCode(db.Model):
         __tablename__ = 'oauth2_code'
@@ -48,12 +49,12 @@ def init_db():
     db.session.execute("""INSERT INTO `oauth2_client` VALUES ('skwIPnbi7N3uIvNysUbi0xfXwnWaIMR1MCJxz8rV0dGxeMJD','0',0,'https://tb-mobile.cirg.washington.edu/redirect','https://tb-mobile.cirg.washington.edu/redirect','email',1,576,'tb-mobile-app','https://tb-mobile.cirg.washington.edu','implicit');""")
     db.session.execute("""INSERT INTO `oauth2_client` VALUES ('pkwIPnbi7N3uIvNysUbi0xfXwnWaIMR1MCJxz8rV0dGxeMJD','0',0,'https://mpower-dev.cirg.washington.edu/mpower_tb-ivanc/auth/truenth/oauth2callback','https://tb-mobile.cirg.washington.edu/auth/truenth/oauth2callback','email',2,576,'mpower-tb','https://mpower-dev.cirg.washington.edu','authorization_code');""")
     db.session.commit()
+    db.session.close()
 
 @app.cli.command('initdb')
 def initdb_command():
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://tbapi:tbapi@tbapp-db:3306'
-    db = SQLAlchemy(app)
-
+    
     db.session.execute('CREATE SCHEMA IF NOT EXISTS tbapi')
+    db.session.close()
     init_db()
     print('Initialized the database.')
